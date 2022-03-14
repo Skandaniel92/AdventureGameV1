65de04e4d798c20a6e1c9f23c153f46432aa7a69
TheOneZephyrus <82367427+TheOneZephyrus@users.noreply.github.com>
2022-03-14 15:27:05 +0100

    ideer til at starte

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Test1");
-        System.out.println("test2");
+        startGame();
+        printMenu();
+
+    }
+    //måske noget vi kan bruge
+    static void UseCommands() {
+        String input = "";
+        Scanner sc = new Scanner(System.in);
+        while (!input.equals("Close")) {
+            input = sc.next();
+            switch (input) {
+
+
+                case "commands":
+                    PlayerActions.ShowCommands();
+                    break;
+                case "inventory":
+                    PlayerActions.ShowInventory();
+                    break;
+                case "equipment":
+                    PlayerActions.ShowEquipment();
+                    break;
+                case "spawnItem":
+                    Item.SpawnItem();
+                    break;
+                case "addItem":
+
+                case "close":
+                    sc.close();
+                    break;
+            }
+        }
+
     }
 }
