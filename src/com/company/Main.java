119bd8aefec36c2d44d72ed002412dcdd762210e
TheOneZephyrus <82367427+TheOneZephyrus@users.noreply.github.com>
2022-03-14 14:57:01 +0100

    chchchanges

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,7 @@ package com.company;
 
 public class Main {
 
-    public void chchchanges() {
-        System.out.println("...");
-    }
+
     public static void main(String[] args) {
 
         System.out.println("Test1");
