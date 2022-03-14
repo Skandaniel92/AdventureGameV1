d5a21d93a8a24a99ace0d027130153ea8391024c
TheOneZephyrus <82367427+TheOneZephyrus@users.noreply.github.com>
2022-03-14 14:55:23 +0100

    chchchanges

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,12 @@ package com.company;
 
 public class Main {
 
+    public void chchchanges() {
+        System.out.println("...");
+    }
     public static void main(String[] args) {
+
         System.out.println("Test1");
+        System.out.println("test2");
     }
 }
