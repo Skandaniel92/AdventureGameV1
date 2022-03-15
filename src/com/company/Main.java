68589f981feb7fe737e638a1376a4e4155889ac7
IamtheZephyrus <nath3639@stud.kea.dk>
2022-03-15 12:08:03 +0100

    måske ser det bedre ud nu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
 
-    public static void main(String[] args) {
-        Room room1 = new Room("1", "blabla");
-        Room room2 = new Room("2", "blabla");
-        Room room3 = new Room("3", "blabla");
-        Room room4 = new Room("4", "blabla");
-        Room room5 = new Room("5", "blabla");
-        Room room6 = new Room("6", "blabla");
-        Room room7 = new Room("7", "blabla");
-        Room room8 = new Room("8", "blabla");
-        Room room9 = new Room("9", "blabla");
-
         startGame();
         printMenu();
+        createPlayer();
 
-    }
-    //måske noget vi kan bruge
     static void UseCommands() {
         String input = "";
         Scanner sc = new Scanner(System.in);
@@ -26,29 +16,16 @@ public class Main {
             input = sc.next();
             switch (input) {
 
-
-                case "commands":
-                    PlayerActions.ShowCommands();
+                case "help":
+                    TUI.help();
                     break;
-                case "inventory":
-                    PlayerActions.ShowInventory();
+                case "look":
+                    TUI.lookAround();
                     break;
-                case "equipment":
-                    PlayerActions.ShowEquipment();
-                    break;
-                case "spawnItem":
-                    Item.SpawnItem();
-                    break;
-                case "addItem":
-
-                case "close":
-                    sc.close();
+                case "exit":
+                    TUI.exit();
                     break;
             }
-        }
-        public void help(){
-            System.out.println("You have the options to either go \n" + "North \n" + "East \n" + "South \n" + "West \n" +
-                    "But be carefull! you have to guess where to go! \n" + "Where do you choose to go?");
         }
 
     }
--- src/com/company/TUI.java
+++ src/com/company/TUI.java
@@ -2,17 +2,23 @@ package com.company;
 
 public class TUI {
 
-    public void help(){
+    public static void printMenu(){
+        System.out.println("Welcome to the game ___ if you wish to start, please create a player");
+        System.out.println("If you wish to exit the game... sad. \n" + "Type exit");
+    }
+
+    public static void help(){
         System.out.println("You have the options to either go \n" + "North \n" + "East \n" + "South \n" + "West \n" +
                 "But be carefull! you have to guess where to go!");
     }
 
-    public void exit(){
+    public static void exit(){
         System.out.println("You are now exiting the game...");
         System.exit(0);
     }
 
-    public void lookAround(){
+    public static void lookAround(){
+        .......
 
 
     }
