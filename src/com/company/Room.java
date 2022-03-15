e0c3a91bc32a21491448a38bfb98f814d7ad1ceb
Skandaniel92 <dani32c4@stud.kea.dk>
2022-03-15 13:24:06 +0100

    CreatingRooms tilføjet

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,13 +1,33 @@
 package com.company;
 
 public class Room {
-    private String name;
-    private String description;
-
+  private String name;
+  private String description;
+  private Room roomWest;
+  private Room roomEast;
+  private Room roomNorth;
+  private Room roomSouth;
 
 
   public Room(String name, String description){
     this.name = name;
     this.description = description;
   }
+
+  public void setWest(Room room){
+    roomWest = room;
+  }
+
+  public void setNorth(Room room){
+    roomNorth = room;
+  }
+
+  public void setEast(Room room){
+    roomEast = room;
+  }
+
+  public void setSouth(Room room){
+    roomSouth = room;
+  }
+
 }
