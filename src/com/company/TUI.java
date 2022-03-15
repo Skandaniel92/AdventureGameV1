36043a2d67cf0da0f434d65c7d95b911e89038a5
IamtheZephyrus <nath3639@stud.kea.dk>
2022-03-15 14:39:44 +0100

    sidste test

--- src/com/company/TUI.java
+++ src/com/company/TUI.java
@@ -1,25 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class TUI {
 
-    public static void printMenu(){
+    public static void printMenu() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the game ___ if you wish to start, please create a player");
         System.out.println("If you wish to exit the game... sad. \n" + "Type exit");
+        String userInput = scanner.nextLine();
+        if (userInput.equals("exit")) {
+            exit();
+        }
+
     }
 
-    public static void help(){
+    public static void help() {
         System.out.println("You have the options to either go \n" + "North \n" + "East \n" + "South \n" + "West \n" +
                 "But be carefull! you have to guess where to go!");
     }
 
-    public static void exit(){
+    public static void exit() {
         System.out.println("You are now exiting the game...");
         System.exit(0);
     }
 
-    public static void lookAround(){
-        .......
-
+    public void lookAround() {
+        System.out.println(currentRoom.getDescription);
 
     }
 
