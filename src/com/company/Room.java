be092c0dd0213f7f64fd95a15b5b045cffe172bb
Skandaniel92 <dani32c4@stud.kea.dk>
2022-03-15 13:17:58 +0100

    Room-klasse tilføjet

---
+++ src/com/company/Room.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Room {
+    private String name;
+    private String description;
+
+
+
+  public Room(String name, String description){
+    this.name = name;
+    this.description = description;
+  }
+}
