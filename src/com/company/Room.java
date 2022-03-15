759c8589fb408a3d57ce68dd89615d248c8bb3a1
IamtheZephyrus <nath3639@stud.kea.dk>
2022-03-15 14:47:58 +0100

    sidste test

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -7,7 +7,7 @@ public class Room {
   private Room roomEast;
   private Room roomNorth;
   private Room roomSouth;
-  private Room currentRoom;
+
 
   public Room(String name, String description) {
     this.name = name;
@@ -42,17 +42,18 @@ public class Room {
     currentRoom = input;
   }
 
-  public void creatingRooms() {
-    Room room1 = new Room("Entrance to the mountain", "You are standing at the entrance to the mountain - decide what you want to do now -->");
-    Room room2 = new Room("First room in the mountain", "You are now inside the mountain. It's dark and cold - decide what you want to do next -->");
-    Room room3 = new Room("The big room", "You are in a very large and warm room. There is a smell of bonfire and you can hear a crackling sound - decide what you want to do next -->");
-    Room room4 = new Room("The cave", "The cave - You have gone down a long stairway and are now in a cave with a lake. You can hear dripping water - decide what you want to do now -->");
-    Room room5 = new Room("The mysterious space ", "You are now in a room with a violet light and strange music - decide what you want to do now -->");
-    Room room6 = new Room("The room with the stairs ", "This room is completely quiet. There is a bit of reddish light and you can see a staircase - decide what you want to do now -->");
-    Room room7 = new Room("The room at the top ", "You have now gone up the stairs, and you are at the top of the mountain - decide what you want to do now -->");
-    Room room8 = new Room("Outside ", "You have now come outside and you can walk around the mountain - decide what you want to do now -->");
-    Room room9 = new Room("At the end of the mountain ", "You have gone down at the end of the mountain - decide what you want to do now -->");
+  Room room1 = new Room("Entrance to the mountain", "You are standing at the entrance to the mountain - decide what you want to do now -->");
+  Room room2 = new Room("First room in the mountain", "You are now inside the mountain. It's dark and cold - decide what you want to do next -->");
+  Room room3 = new Room("The big room", "You are in a very large and warm room. There is a smell of bonfire and you can hear a crackling sound - decide what you want to do next -->");
+  Room room4 = new Room("The cave", "The cave - You have gone down a long stairway and are now in a cave with a lake. You can hear dripping water - decide what you want to do now -->");
+  Room room5 = new Room("The mysterious space ", "You are now in a room with a violet light and strange music - decide what you want to do now -->");
+  Room room6 = new Room("The room with the stairs ", "This room is completely quiet. There is a bit of reddish light and you can see a staircase - decide what you want to do now -->");
+  Room room7 = new Room("The room at the top ", "You have now gone up the stairs, and you are at the top of the mountain - decide what you want to do now -->");
+  Room room8 = new Room("Outside ", "You have now come outside and you can walk around the mountain - decide what you want to do now -->");
+  Room room9 = new Room("At the end of the mountain ", "You have gone down at the end of the mountain - decide what you want to do now -->");
+  Room currentRoom = room1;
 
+  public void creatingRooms() {
     room1.setEast(room2);
     room1.setSouth(room4);
     room2.setWest(room1);
